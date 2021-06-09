5d69a74719b2f6f4454528567e6140e4aa72ce98
Mohamed Gallab <moknaruto@gmail.com>
2021-06-09 17:13:39 +0200

    first commit

--- General Part.pl
+++ General Part.pl
@@ -1,3 +1,4 @@
+%general part
 convertBinToDec(Bin,Dec):-
     convertBinToDec(Bin,Dec,0).
 convertBinToDec(0, 0, _).
@@ -52,4 +53,71 @@ fillZeros(String,N,R):-
     N1 is N - 1,
     fillZeros(StringNew,N1,R).
 
-%hello
+%component 1
+
+%component 2
+
+%component 3
+
+tagToString(MemoryWord,BitsNum,Tag,StrTag):-
+    atom_length(MemoryWord, StrLen),
+    atom_number(ShortTag, Tag),
+    atom_length(ShortTag, ShortTagLen),
+    TagLen is StrLen - ShortTagLen - BitsNum,
+    fillZeros(ShortTag, TagLen, StrTag).
+
+searchCache(OldCache,Indx):-
+    searchCache(OldCache,0, Indx);
+    searchCache(OldCache,-1,-1,0,Indx).
+
+
+searchCache([item(tag(_), data(_), 0, _)|_],Acc,Acc).
+searchCache([item(tag(_), data(_), Vbit, _)|T],Acc,Indx):-
+    Vbit \= 0,
+    NewAcc is Acc + 1,
+    searchCache(T, NewAcc, Indx).
+
+searchCache([],_,BgstIndx,_,BgstIndx).
+
+searchCache([item(tag(_), data(_), Vbit, Obit)|T],Bgst,_,Acc,Indx):-
+    Vbit \= 0,
+    Obit > Bgst,
+    NewAcc is Acc + 1,
+    searchCache(T,Obit,Acc,NewAcc,Indx).
+
+searchCache([item(tag(_), data(_), Vbit, Obit)|T],Bgst,BgstIndx,Acc,Indx):-
+    Vbit \= 0,
+    Obit < Bgst,
+    NewAcc is Acc + 1,
+    searchCache(T,Bgst,BgstIndx,NewAcc,Indx).
+
+incrementCache([],[]).
+incrementCache([item(Tag, Data, 1, Obit)|T], [item(Tag, Data, 1, ObitNew)|Tn]):-
+    ObitNew is Obit + 1,
+    incrementCache(T, Tn).
+incrementCache([item(Tag, Data, 0, Obit)|T], [item(Tag, Data, 0, Obit)|Tn]):-
+    incrementCache(T, Tn).
+
+replaceInCache(Tag,Idx,Mem,OldCache,NewCache,ItemData,directMap,BitsNum):-
+    convertBinToDec(Idx,I),
+    MemAdrsBin is Tag * (10**BitsNum) + Idx,
+    convertBinToDec(MemAdrsBin,MemAdrs),
+    nth0(MemAdrs, Mem, ItemData),
+    tagToString(ItemData,BitsNum,Tag,FinalTag),
+    replaceIthItem(item(tag(FinalTag),data(ItemData),1,0),OldCache,I,NewCache).
+
+replaceInCache(Tag,0,Mem,OldCache,NewCache,ItemData,fullyAssoc,_):-
+    convertBinToDec(Tag, MemAdrs),
+    nth0(MemAdrs, Mem, ItemData),
+    tagToString(ItemData,0,Tag,FinalTag),
+    searchCache(OldCache,Indx),
+    incrementCache(OldCache, MidCache),
+    replaceIthItem(item(tag(FinalTag),data(ItemData),1,0),MidCache,Indx,NewCache).
+
+/*
+replaceInCache(Tag,Idx,Mem,OldCache,NewCache,ItemData,setAssoc,SetsNum):-
+    convertBinToDec(Idx,I),
+    MemAdrsBin is Tag * (10**BitsNum) + Idx,
+    convertBinToDec(MemAdrsBin,MemAdrs),
+    nth0(MemAdrs, Mem, ItemData),
+    tagToString(ItemData,BitsNum,Tag,FinalTag),*/
