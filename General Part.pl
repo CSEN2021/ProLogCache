c4003854db82deddfb45c6cddfe9d20c6a0c8109
Mohamed Gallab <moknaruto@gmail.com>
2021-06-10 23:25:58 +0200

    pre made predicates added

--- General Part.pl
+++ General Part.pl
@@ -53,7 +53,7 @@ fillZeros(String,N,R):-
     N1 is N - 1,
     fillZeros(StringNew,N1,R).
 
-%component 1
+%component 1 & 2
 
 getDataFromCache(StringAddress,Cache,Data,0,directMap,BitsNum):-
     getIndexTag(StringAddress,BitsNum,Index,Tag),
@@ -62,11 +62,25 @@ getDataFromCache(StringAddress,Cache,Data,0,directMap,BitsNum):-
     nth0(Ind, Cache, Item),
     Item=item(tag(Tag),data(Data),1,_).
 
+getDataFromCache(StringAddress,Cache,Data,HopsNum,setAssoc,SetsNum):-
+    getNumBits(SetsNum,setAssoc,_,BitsNum),
+    getIndexTag(StringAddress,BitsNum,Index,Tag),
+    atom_number(Index, IndexBin),
+    convertBinToDec(IndexBin,IndexDecimal),
+    length(Cache,CacheLength),
+    AddressPerSet is CacheLength // SetsNum,
+    splitEvery(AddressPerSet, Cache, Sets),
+    nth0(IndexDecimal, Sets, Set),
+    findTagInSet(Tag, Set, Data, 0, HopsNum).
+
 convertAddress(Bin,BitsNum,Tag,Idx,directMap):-
     Idx is Bin mod (10 ** BitsNum),
     Tag is Bin // (10 ** BitsNum).
 
-%component 2
+convertAddress(Bin,SetsNum,Tag,Idx,setAssoc):-
+    getNumBits(SetsNum,setAssoc,_,BitsNum),
+    Idx is Bin mod (10 ** BitsNum),
+    Tag is Bin // (10 ** BitsNum).
 
 getIndexTag(StringAddress,BitsNum,Index,Tag):-
     string_length(StringAddress,StringLength),
@@ -85,25 +99,7 @@ getIndexTag(StringAddress,BitsNum,Index,Tag):-
     
     fillZeros(StringIndex,Diff,Index).
     
-    
-    
-convertAddress(Bin,SetsNum,Tag,Idx,setAssoc):-
-    getNumBits(SetsNum,setAssoc,_,BitsNum),
-    Idx is Bin mod (10 ** BitsNum),
-    Tag is Bin // (10 ** BitsNum).
-    
-getDataFromCache(StringAddress,Cache,Data,HopsNum,setAssoc,SetsNum):-
-    getNumBits(SetsNum,setAssoc,_,BitsNum),
-    getIndexTag(StringAddress,BitsNum,Index,Tag),
-    atom_number(Index, IndexBin),
-    convertBinToDec(IndexBin,IndexDecimal),
-    length(Cache,CacheLength),
-    AddressPerSet is CacheLength // SetsNum,
-    splitEvery(AddressPerSet, Cache, Sets),
-    nth0(IndexDecimal, Sets, Set),
-    findTagInSet(Tag, Set, Data, 0, HopsNum).
-
-findTagInSet(Tag, [item(tag(Tag), Data, 1, _)|_], Data, Acc, Acc).
+findTagInSet(Tag, [item(tag(Tag), data(Data), 1, _)|_], Data, Acc, Acc).
     
 findTagInSet(Tag,[item(tag(Tag2),_,_,_)|T], Data, Acc, HopsNum):-
     Tag \= Tag2,
@@ -161,13 +157,13 @@ replaceInCache(Tag,Idx,Mem,OldCache,NewCache,ItemData,directMap,BitsNum):-
     MemAdrsBin is Tag * (10**BitsNum) + Idx,
     convertBinToDec(MemAdrsBin,MemAdrs),
     nth0(MemAdrs, Mem, ItemData),
-    tagToString(ItemData,BitsNum,Tag,FinalTag),
+    tagToString("123456",BitsNum,Tag,FinalTag),
     replaceIthItem(item(tag(FinalTag),data(ItemData),1,0),OldCache,I,NewCache).
 
 replaceInCache(Tag,0,Mem,OldCache,NewCache,ItemData,fullyAssoc,_):-
     convertBinToDec(Tag, MemAdrs),
     nth0(MemAdrs, Mem, ItemData),
-    tagToString(ItemData,0,Tag,FinalTag),
+    tagToString("123456",0,Tag,FinalTag),
     searchCache(OldCache,Indx),
     incrementCache(OldCache, MidCache),
     replaceIthItem(item(tag(FinalTag),data(ItemData),1,0),MidCache,Indx,NewCache).
@@ -178,7 +174,7 @@ replaceInCache(Tag,Idx,Mem,OldCache,NewCache,ItemData,setAssoc,SetsNum):-
     MemAdrsBin is Tag * (10**BitsNum) + Idx,
     convertBinToDec(MemAdrsBin,MemAdrs),
     nth0(MemAdrs, Mem, ItemData),
-    tagToString(ItemData,BitsNum,Tag,FinalTag),
+    tagToString("123456",BitsNum,Tag,FinalTag),
     length(OldCache, CacheLen),
     SetSize is CacheLen // SetsNum,
     splitEvery(SetSize, OldCache, ListedCache),
@@ -188,5 +184,23 @@ replaceInCache(Tag,Idx,Mem,OldCache,NewCache,ItemData,setAssoc,SetsNum):-
     replaceIthItem(item(tag(FinalTag),data(ItemData),1,0),IncSet,Indx,NewSet),
     replaceIthItem(NewSet,ListedCache,I,NewListedCache),
     sumListedList(NewListedCache, [], NewCache).
+
+% Pre-Implemented Predicates
+
+getData(StringAddress,OldCache,Mem,NewCache,Data,HopsNum,Type,BitsNum,hit):-
+    getDataFromCache(StringAddress,OldCache,Data,HopsNum,Type,BitsNum),
+    NewCache = OldCache.
+getData(StringAddress,OldCache,Mem,NewCache,Data,HopsNum,Type,BitsNum,miss):-
+    \+getDataFromCache(StringAddress,OldCache,Data,HopsNum,Type,BitsNum),
+    atom_number(StringAddress,Address),
+    convertAddress(Address,BitsNum,Tag,Idx,Type),
+    replaceInCache(Tag,Idx,Mem,OldCache,NewCache,Data,Type,BitsNum).
+
+runProgram([],OldCache,_,OldCache,[],[],Type,_).
+runProgram([Address|AdressList],OldCache,Mem,FinalCache,[Data|OutputDataList],[Status|StatusList],Type,NumOfSets):-
+    getNumBits(NumOfSets,Type,OldCache,BitsNum),
+    (Type = setAssoc, Num = NumOfSets; Type \= setAssoc, Num = BitsNum),
+    getData(Address,OldCache,Mem,NewCache,Data,HopsNum,Type,Num,Status),
+    runProgram(AdressList,NewCache,Mem,FinalCache,OutputDataList,StatusList,Type,NumOfSets).
 
 
